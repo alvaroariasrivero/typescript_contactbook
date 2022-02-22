a92a41cf01705f0e6e2ea5746f9c9569c541bf14
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-02-22 13:34:36 +0100

    class to functional

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import * as React from "react";
 
 type FormProps = {
   //
 };
 
-class Form extends Component<FormProps> {
-  public render() {
-    return <form>
-      <label htmlFor="name">Name:</label>
-      <input type="text" name="name"/>
-      <label htmlFor="surname">Surname:</label>
-      <input type="text" name="surname"/>
-      <label htmlFor="phone">Phone number:</label>
-      <input type="number" name="phone"/>
-      <label htmlFor="img">Picture URL:</label>
-      <input type="text" name="img"/>
-      <input type="submit" value="Save"/>
-    </form>;
-  }
-}
+const Form: React.FC<any> = () => {
+  return <form>
+  <label htmlFor="name">Name:</label>
+  <input type="text" name="name"/>
+  <label htmlFor="surname">Surname:</label>
+  <input type="text" name="surname"/>
+  <label htmlFor="phone">Phone number:</label>
+  <input type="number" name="phone"/>
+  <label htmlFor="img">Picture URL:</label>
+  <input type="text" name="img"/>
+  <input type="submit" value="Save"/>
+</form>;
+};
 
 export default Form;
